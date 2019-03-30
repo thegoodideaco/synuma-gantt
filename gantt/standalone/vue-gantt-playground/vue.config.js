d63c152579d05d137720adb75de223be47761146
Jonathan Snyder <jon.jsstudios@gmail.com>
2019-03-29 23:33:26 -0400

    slight fixes

---
+++ gantt/standalone/vue-gantt-playground/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lintOnSave:          undefined,
+  publicPath:          undefined,
+  outputDir:           undefined,
+  assetsDir:           undefined,
+  runtimeCompiler:     true,
+  productionSourceMap: undefined,
+  parallel:            undefined,
+
+  css: {
+    sourceMap: true
+  }
+}
