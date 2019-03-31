4563797167161d8c6c77b55222f244f406d42099
Jonathan Snyder <jon.jsstudios@gmail.com>
2019-03-30 21:27:53 -0400

    style fixes

--- gantt/standalone/vue-gantt-playground/src/App.vue
+++ gantt/standalone/vue-gantt-playground/src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">
-        Home
-      </router-link> |
-      <router-link to="/about">
-        About
-      </router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -17,14 +9,18 @@
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  height: 100%;
+  > div {
+    height: 100%;
+    display: grid;
+    grid: auto 1fr / 100%;
+
+    > * {
+      &:nth-child(2) {
+        overflow: auto;
+        padding: 10px;
+        background-color: #1c4b58;
+      }
     }
   }
 }
--- gantt/standalone/vue-gantt-playground/src/views/Home.vue
+++ gantt/standalone/vue-gantt-playground/src/views/Home.vue
@@ -1,23 +1,71 @@
 <template>
   <div>
-    Make a gantt chart
+    <!-- Basic Info -->
+    <div v-if="basicInfo"
+         class="info-bar">
+      <h2>
+        {{ basicInfo.timelineName }} /
+        <small>{{ totalTime }}</small>
+      </h2>
 
+      <!-- Include Weekends -->
+      <label>
+        <input type="checkbox"
+               :value="basicInfo.includeWeekends">
+
+        <span>Include Weekends</span>
+      </label>
+
+      <!-- Landlord Turnover Date -->
+      <label>
+        <input type="checkbox"
+               :value="basicInfo.includeWeekends">
+
+        <span>Has Landlord Turnover Date</span>
+      </label>
+
+      <!-- Display Timeline Planned Date -->
+      <label>
+        <input type="checkbox"
+               :value="basicInfo.includeWeekends">
+
+        <span>Display Timeline Planned Date</span>
+      </label>
+    </div>
+
+    <!-- Content -->
     <div>
-      <ul>
+      <ul class="phase-list">
+        <!-- Phases -->
         <li v-for="item in phases"
             :key="item.name">
-          <span :title="item | stringify">
+          <span :title="item | stringify"
+                @click="showdata(item)">
 
-            {{ item.name }}
+            {{ item.name }} - {{ item.startDate | formatDate }}
           </span>
 
+          <!-- Phase Milestones -->
           <ul>
             <li v-for="(milestone, index) in item.milestones"
                 :key="index">
-              <span :title="milestone | stringify">
+              <span :title="milestone | stringify"
+                    @click="showdata(milestone)">
 
                 {{ milestone.name }}
               </span>
+
+              <!-- Tasks -->
+              <ul>
+                <li v-for="(task, taskIndex) in milestone.tasks"
+                    :key="taskIndex">
+                  <span :title="task | stringify"
+                        @click="showdata(task)">
+
+                    {{ task.name }} / {{ task.statusName }}
+                  </span>
+                </li>
+              </ul>
             </li>
           </ul>
         </li>
@@ -30,13 +78,13 @@
 import * as d3 from 'd3'
 
 export default {
+  name:    'Home',
   filters: {
     stringify(val) {
       return JSON.stringify(val, null, ' ')
     },
     formatDate(dateStr) {
-      const f = d3.timeFormat('mm:MM')(new Date(dateStr))
-      d3.area
+      const f = d3.timeFormat('%A, %b %d, %Y at %X')(new Date(dateStr))
       return f
     }
   },
@@ -45,7 +93,10 @@ export default {
       /**
        * @type {GanttDataObject}
        */
-      result: null
+      result: null,
+
+      /** @type {number[]} */
+      dateRange: null
     }
   },
   computed: {
@@ -53,24 +104,125 @@ export default {
       if (this.result) {
         return this.result.phases
       }
+    },
+    basicInfo() {
+      if (!this.result) return null
+      const {
+        timelineName,
+        includeWeekends,
+        hasLandlordTurnoverDate,
+        displayTimelinePlannedDate
+      } = this.result
+
+      return {
+        timelineName,
+        includeWeekends,
+        hasLandlordTurnoverDate,
+        displayTimelinePlannedDate
+      }
+    },
+    totalTime() {
+      if(this.dateRange) {
+        const [b, e] = d3.extent(this.dateRange)
+
+        const ms = e - b
+        const s = ms / 1000 >> 0
+        const m = s / 60 >> 0
+        const h = m / 60 >> 0
+        const d = h / 24 >> 0
+
+        const labels = ['seconds', 'minutes', 'hours', 'days']
+        const values = [
+          s % 60,
+          m % 60,
+          h % 24,
+          d % 365
+        ]
+
+        return values.map((v, i)=> {
+          return `${v} ${labels[i]}`
+        }).reverse().join(', ')
+      }
     }
   },
   async mounted() {
     /** @type {{data: GanttDataObject}} */
     const { data } = await d3.json('/data.json')
-
     this.result = data
+
+    this.dateRange = this.getAllDates()
   },
   methods: {
-    generate() {
-      const {
-        phases
-      } = this.result
+    getAllDates() {
 
-      phases.forEach(v => {
-        v.
+
+
+      /** @type {number[]} */
+      const all = []
+
+      /** @type {GanttDataObject | Phase | Milestone | Task} */
+      const obj = JSON.parse(JSON.stringify(this.result))
+
+      const rootNode = d3.hierarchy(obj, v => v.phases || v.milestones || v.tasks)
+
+      rootNode.eachBefore(v => {
+        const dates = Object.entries(v.data).reduce((prev, cur) => {
+          const [key, val] = cur
+
+          if(key.match(/date/gi) && typeof val === 'string') {
+            const ms = Date.parse(val)
+            prev.push(ms)
+          }
+
+          return prev
+        }, [])
+
+        all.push(...dates)
       })
+
+      return all
+    },
+    showdata(item) {
+      console.log(JSON.stringify(item, null, ' '))
     }
   }
 }
 </script>
+
+<style lang="scss">
+.info-bar {
+  background-color: #007292;
+  color: #fdfdfd;
+  text-shadow: -1px -1px 3px rgba(0, 0, 0, 0.39);
+  padding: 10px 15px;
+  pointer-events: none;
+  display: grid;
+  align-items: center;
+  grid-auto-flow: column dense;
+  grid-template-columns: 1fr repeat(3, minmax(100px, max-content));
+  gap: 25px;
+}
+
+.phase-list {
+  // list-style: none;
+  padding: 0;
+  margin: 0;
+  display: grid;
+  row-gap: 20px;
+
+  ul {
+    padding-left: 10px;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+    background-color: #fafafa;
+    padding: 5px;
+
+    &:nth-child(odd) {
+      background-color: #fafafa;
+    }
+  }
+}
+</style>
