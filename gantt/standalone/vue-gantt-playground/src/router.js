adf6b2d9a29a4746b7fa6c790b294ea61f2f9a2a
Jonathan Snyder <jon.jsstudios@gmail.com>
2019-04-01 23:52:30 -0400

    minor fixes

--- gantt/standalone/vue-gantt-playground/src/router.js
+++ gantt/standalone/vue-gantt-playground/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Gantt from './views/Gantt.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
     {
       path:      '/',
       name:      'home',
+      component: About
+    },
+    {
+      path:      '/new',
+      name:      'new',
       component: Home
     },
     {
--- gantt/standalone/vue-gantt-playground/src/views/About.vue
+++ gantt/standalone/vue-gantt-playground/src/views/About.vue
@@ -1,5 +1,86 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="pt-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <div class="form-check">
+            <input id="plannedTimelines"
+                   class="form-check-input"
+                   type="checkbox"
+                   value="">
+            <label class="form-check-label"
+                   for="plannedTimelines">
+              Show planned timelines
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="metadata border-right">
+        <div class="col-headers">
+          <!-- TBD figure this shit out -->
+        </div>
+        <div class="row mt-4">
+          <div class="col-3">
+            <div class="gantt-label in-progress">
+              In Progress
+            </div>
+          </div>
+          <div class="col phase">
+            <i class="fas fa-angle-down mr-2" />
+            Real Estate
+            <div class="row milestone align-items-center">
+              <div class="col">
+                <i class="fas fa-check-circle mr-2 text-green" />Submit LOI
+              </div>
+              <div class="col task-list">
+                3 of 3 tasks complete
+              </div>
+              <div class="col assigned-to">
+                Justin Davis
+              </div>
+            </div>
+            <div class="row milestone align-items-center">
+              <div class="col">
+                <i class="fas fa-check-circle mr-2 text-light" />Submit LOI
+              </div>
+              <div class="col task-list">
+                3 of 3 tasks complete
+              </div>
+              <div class="col assigned-to">
+                Justin Davis
+              </div>
+            </div>
+            <div class="row milestone align-items-center">
+              <div class="col">
+                <i class="fas fa-check-circle mr-2 text-light" />Submit LOI
+              </div>
+              <div class="col task-list">
+                3 of 3 tasks complete
+              </div>
+              <div class="col assigned-to">
+                Justin Davis
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-4 align-items-center">
+          <div class="col-3">
+            <div class="gantt-label not-started">
+              Not Started
+            </div>
+          </div>
+          <div class="col phase">
+            <i class="fas fa-angle-right mr-2" />
+            Real Estate
+          </div>
+        </div>
+      </div> <!-- end .metadata -->
+      <div class="date-grid">
+        <div class="row">
+          <span>1</span>
+          <span>2</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
